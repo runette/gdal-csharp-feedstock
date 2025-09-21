fd0c9f09ea98f5e39abeeb13547dfd9045bb18d6
Paul <runette@gmail.com>
2025-09-21 14:49:39 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ unset PYTHON
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 7.0
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 9.0
 
 export PATH=$PATH:~/.dotnet
 
